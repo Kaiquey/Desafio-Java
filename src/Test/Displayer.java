af28da422e1cc72c1e2980c6da1dff3b02c21bc5
Kaique.Dev <105029198+Kaiquey@users.noreply.github.com>
2025-07-02 23:34:47 -0300

    Update Displayer.java

--- src/Test/Displayer.java
+++ src/Test/Displayer.java
@@ -9,10 +9,10 @@ public class Displayer {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int [] num = new int [4];
-		System.out.println("Digite uns número ai");
+		System.out.println("Digite um número: ");
 		for(int i=0; i < num.length; i++) {System.out.print("Número " + (i + 1) + ": ");num[i] = scanner.nextInt();}
 		Arrays.sort(num);
-		System.out.println("Os números alinhados pae: ");
+		System.out.println("Os números alinhados: ");
 		for(int number : num) {System.out.println(number);}
 		System.out.println("----");
 		System.out.println("----");
